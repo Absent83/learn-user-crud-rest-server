ae165c36f0318435e763f88471edb9490e7833e3
Nikolay Dolgopolov <nickolanick26@gmail.com>
2019-05-17 19:08:51 +0300

    With SpringBoot Security

--- src/main/java/com/myhome/springCrudRestServer/configuration/SecurityConfig.java
+++ src/main/java/com/myhome/springCrudRestServer/configuration/SecurityConfig.java
@@ -1,18 +1,56 @@
 package com.myhome.springCrudRestServer.configuration;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-///**
-// * @author Nick Dolgopolov (nick_kerch@mail.ru; https://github.com/Absent83/)
-// */
-//
-////@Configuration
-////@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-////todo
-//
-//}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Nick Dolgopolov (nick_kerch@mail.ru; https://github.com/Absent83/)
+ */
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        System.out.println("=== SecurityConfig === === configure AuthenticationManagerBuilder ===");
+
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("adminpassword").roles("ADMIN");
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+
+                //без сессий (для REST)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+
+                //запрашивает login-password из head (открывает окно аутентификации)
+                .httpBasic();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
 
